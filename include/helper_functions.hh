a25cd58e2d2ec3bca4a4d52c3749d9842eba1a6a
Christian Reisinger <reisinger@itp.uni-frankfurt.de>
2018-08-14 17:11:50 +0200

    Added function indexOf(const std::vector<T>, const T elem)

--- include/helper_functions.hh
+++ include/helper_functions.hh
@@ -5,6 +5,7 @@
  *      Author: reisinger
  */
 #include <memory>
+#include <vector>
 
 #ifndef INCLUDE_HELPER_FUNCTIONS_HH_
 #define INCLUDE_HELPER_FUNCTIONS_HH_
@@ -12,6 +13,11 @@
 template<typename T, typename ...Args>
 std::unique_ptr<T> make_unique(Args&& ...args) {
 	return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+
+template<typename T>
+int indexOf(const std::vector<T>& v, const T elem) {
+	return (int) distance(v.begin(), find(v.begin(), v.end(), elem));
 }
 
 #endif /* INCLUDE_HELPER_FUNCTIONS_HH_ */
