910e343c43b18dd3ba7d3ca25b0c8a26885afd8f
Christian Reisinger <reisinger@itp.uni-frankfurt.de>
2018-10-18 14:44:29 +0200

    New tool (bin) "toranges"
    * first executable: adjusted Makefile accordingly

--- Makefile
+++ Makefile
@@ -4,14 +4,20 @@ INCLUDE=-I"include"
 
 SRC_DIR=src/
 BUILD_DIR=lib/
+BIN_DIR=bin/
 NAMES=linalg_io
+BIN_NAMES=toranges
 
 OBJS=${NAMES:%=${BUILD_DIR}/lib%.so}
+BINS=${BIN_NAMES:%=${BIN_DIR}/%}
 
-all: ${OBJS}
+all: ${OBJS} ${BINS}
 
 ${BUILD_DIR}/lib%.so: ${SRC_DIR}/%.cc
 	${CXX} ${CXXFLAGS} -c -o $@ $< ${INCLUDE}
+	
+${BINS}: ${BIN_DIR}/%: ${SRC_DIR}/%.cc
+	${CXX} ${CXXFLAGS} -o $@ $^
 	
 clean:
 	rm -f ${BUILD_DIR}/*
---
+++ src/toranges.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <sstream>
+
+int main(int argc, char **argv) {
+
+	if (argc != 2) {
+		std::cerr << "Usage: " << argv[0] << " <list>\n"
+				"<list> ist a list of ints separated by a single non-number char.\n";
+	}
+	std::istringstream list_iss(argv[1]);
+	std::ostringstream ranges_iss;
+
+	int first, prev, next;
+	char sep;
+	list_iss >> first;
+	ranges_iss << first;
+	prev = first;
+
+	while (list_iss >> sep >> next) {
+//		std::cout << first << " " << prev << " " << next << " " << ranges_iss.str() << "\n";
+		if (next != prev + 1) {
+			if (prev != first)
+				ranges_iss << ".." << prev;
+			ranges_iss << " " << next;
+			first = next;
+		}
+		prev = next;
+	}
+
+	if(prev != first)
+		ranges_iss << ".." << prev;
+
+	std::cout << ranges_iss.str();
+}
