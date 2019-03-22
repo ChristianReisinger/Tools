d1df0b3b8d49385dec478d6b2234606ec60733c5
Christian Reisinger <reisinger@itp.uni-frankfurt.de>
2019-03-22 19:21:47 +0100

    math_functions: added function fit_constant
    * updated Makefile

--- include/math_functions.hh
+++ include/math_functions.hh
@@ -6,6 +6,8 @@
  */
 
 #include <cmath>
+#include <vector>
+#include <utility>
 
 #ifndef INCLUDE_MATH_FUNCTIONS_HH_
 #define INCLUDE_MATH_FUNCTIONS_HH_
@@ -27,6 +29,13 @@ int sgn(double d) {
 int sgn(int i) {
 	return (i > 0) - (i < 0);
 }
+
+/**
+ * Fit a constant to the given data points. Optionally compute reduced chi squared.
+ * Does not compute an error (use e.g. a jackknife analysis).
+ */
+double fit_constant(std::vector<std::pair<double, double> > pts_mu_sigma);
+double fit_constant(std::vector<std::pair<double, double> > pts_mu_sigm, double& red_chi_sq);
 
 }
 
---
+++ src/math_functions.cc
@@ -0,0 +1,35 @@
+/*
+ * math_function.cc
+ *
+ *  Created on: 22.03.2019
+ *      Author: Shiwayari
+ */
+
+#include <cmath>
+#include <vector>
+#include <utility>
+
+#include <math_functions.hh>
+
+double math_functions::fit_constant(std::vector<std::pair<double, double> > pts_mu_sigma) {
+	double fit = 0.0;
+	double norm = 0.0;
+	for (auto pt : pts_mu_sigma) {
+		double sigma_sq = std::pow(pt.second, 2.0);
+		norm += 1.0 / sigma_sq;
+		fit += pt.first / sigma_sq;
+	}
+	return fit / norm;
+}
+
+double math_functions::fit_constant(std::vector<std::pair<double, double> > pts_mu_sigma, double& red_chi_sq) {
+	double fit = fit_constant(pts_mu_sigma);
+
+	int N = pts_mu_sigma.size();
+	red_chi_sq = 0.0;
+	for (auto pt : pts_mu_sigma)
+		red_chi_sq += std::pow(pt.first - fit, 2.0) / std::pow(pt.second, 2.0);
+	red_chi_sq /= (double) N - 1.0;
+
+	return fit;
+}
