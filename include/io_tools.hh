9c77328b7c18c5085708f3d55293a8442f75beb2
Christian Reisinger <reisinger@itp.uni-frankfurt.de>
2019-09-03 18:28:37 +0200

    Change stringstream to any stream in io_tools getline_iss, operator>> and add operator<<

--- include/io_tools.hh
+++ include/io_tools.hh
@@ -10,13 +10,15 @@ namespace io_tools {
 template<typename T>
 void consume_until(std::istream& data, std::function<bool(T)> condition);
 
-std::istream& getline_iss(std::istream& from, std::istringstream& to);
+std::istream& getline_iss(std::istream& from, std::ostream& to);
 
-std::istream& getline_iss(std::istream& from, std::istringstream& to, char delim);
+std::istream& getline_iss(std::istream& from, std::ostream& to, char delim);
 
 }
 
-std::istringstream& operator>>(std::istringstream& from, std::istringstream& to);
+std::istream& operator>>(std::istream& from, std::ostream& to);
+
+std::ostream& operator<<(std::ostream& to, std::istream& from);
 
 #include <detail/io_tools.tcc>
 
--- src/io_tools.cc
+++ src/io_tools.cc
@@ -4,20 +4,27 @@
 
 #include <io_tools.hh>
 
-std::istream& io_tools::getline_iss(std::istream& from, std::istringstream& to) {
+std::istream& io_tools::getline_iss(std::istream& from, std::ostream& to) {
 	return getline_iss(from, to, from.widen('\n'));
 }
 
-std::istream& io_tools::getline_iss(std::istream& from, std::istringstream& to, char delim) {
+std::istream& io_tools::getline_iss(std::istream& from, std::ostream& to, char delim) {
 	std::string str;
 	std::getline(from, str, delim);
-	to = std::istringstream(str);
+	to << str;
 	return from;
 }
 
-std::istringstream& operator>>(std::istringstream& from, std::istringstream& to) {
+std::istream& operator>>(std::istream& from, std::ostream& to) {
 	std::string str;
 	from >> str;
-	to = std::istringstream(str);
+	to << str;
 	return from;
+}
+
+std::ostream& operator<<(std::ostream& to, std::istream& from) {
+	std::string str;
+	from >> str;
+	to << str;
+	return to;
 }
